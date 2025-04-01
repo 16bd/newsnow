9cd9d5ab70232b61df33046e6751c3db35419093
百搭 <75002009+16bd@users.noreply.github.com>
2025-04-01 17:58:31 +0800

    Update index.tsx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -46,7 +46,7 @@ export function Header() {
         <Link to="/" className="flex gap-2 items-center">
           <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/favicon.ico)" }} />
           <span className="text-2xl font-brand line-height-none!">
-            <p>百搭News</p>
+            <p>百搭</p>
           </span>
         </Link>
       </span>
